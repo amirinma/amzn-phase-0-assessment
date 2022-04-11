1ade0ac89212b67a955300090255f30820a1064a
Mohammad <zia.ro@live.com>
2022-04-10 20:08:02 -0400

    Completed assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,9 +1,29 @@
 // 1. Write your functions here
 
+const katzDeli = []
+function takeANumber(array, name){
+  katzDeli.push(name)
+  const newArray = [...katzDeli]
+  const index = newArray.findIndex(nown => nown === name);
+  console.log(`Welcome, ${name}. You are number ${(index + 1)} in line.`)
+}
 
-// 2. Example Usage
 
-// const katzDeli = []
+function line(array){
+     let message = "The line is currently: "
+     for(let i = 0; i < array.length; i++){
+         message += [i + 1] + ". " + array[i] + " "
+     }
+     console.log(message)
+}
+
+
+function nowServing(array){
+  console.log(`Currently serving ${array.shift(0)}.`)
+}
+
+
+
 
 // takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
 // takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
